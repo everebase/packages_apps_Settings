0d777d74757dc97c89056668e7762cddad717b8b
Sagar Rokade <sagarrokade006@gmail.com>
2018-11-17 18:55:59 +0000

    Add Eve-OS In Settings

---
+++ src/com/android/settings/deviceinfo/firmwareversion/EveVersionDialogController.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.deviceinfo.firmwareversion;
+import android.content.Context;
+import android.os.SystemProperties;
+import android.support.annotation.VisibleForTesting;
+import com.android.settings.R;
+public class EveVersionDialogController {
+    @VisibleForTesting
+    private static final int EVE_VERSION_VALUE_ID = R.id.eve_version;
+    private static final String EVE_VERSION = "eve_version";
+    private static final String EVE_PROP = "ro.eve.version";
+    private final FirmwareVersionDialogFragment mDialog;
+    private final Context mContext;
+    public EveVersionDialogController(FirmwareVersionDialogFragment dialog) {
+        mDialog = dialog;
+        mContext = dialog.getContext();
+    }
+     public void initialize() {
+         mDialog.setText(EVE_VERSION_VALUE_ID, SystemProperties.get(EVE_PROP,
+                mContext.getResources().getString(R.string.device_info_default)));
+    }
+}
